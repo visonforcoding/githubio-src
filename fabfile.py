d35daf0529ec6df699ca0e623ed2540dfc7d94ab
vison.cao <vison.cao@huolala.cn>
2021-12-20 13:58:22 +0800

    update blog src

--- fabfile.py
+++ fabfile.py
@@ -94,5 +94,6 @@ def gh_pages():
     # local("cd content && git add . && git commit -m 'update md' && git push origin master && cd ../")
     local("git add . && git commit -m 'update blog src' && git push origin master --force")
     local("cp -rf ./public/* ../visonforcoding.github.io/ && cd ../visonforcoding.github.io && git add . && git commit -m 'update blog' && git push origin master --force")
+    local("pwd")
     local("git push gitee master --force")
 
